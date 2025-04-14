7a3028280c03107521bde6ca329df99c9c6a21d5
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-14 15:15:11 +0200

    solution

--- src/main/scala/Pangrams.scala
+++ src/main/scala/Pangrams.scala
@@ -1,3 +1,3 @@
 object Pangrams:
    def isPangram(input: String): Boolean =
-      input.filter(_.isLetter).map(_.toLower).distinct.length == 26 && input != ""
+      input.filter(_.isLetter).map(_.toLower).distinct.length == 26
