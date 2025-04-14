eabc6debdd435907cf975d88ad671b79d7b4caee
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-14 15:09:16 +0200

    solution

--- src/main/scala/Pangrams.scala
+++ src/main/scala/Pangrams.scala
@@ -1,4 +1,3 @@
-object Pangrams {
-  def isPangram(input: String): Boolean = ???
-}
-
+object Pangrams:
+   def isPangram(input: String): Boolean =
+      input.filter(_.isLetter).map(_.toLower).distinct.length == 26 && input != ""
